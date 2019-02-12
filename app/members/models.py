fc51b12cb1e2ae0462c23b8524e1bce090031403
Jeon Young Hoon <hungyb0924@gmail.com>
2019-02-12 21:58:39 +0900

    프로젝트 진행중.... 모델 생성 및 크롤링 데이터 인스턴스 생성중1

--- app/members/models.py
+++ app/members/models.py
@@ -6,9 +6,6 @@ class User(AbstractUser):
     pass
 
 
-def team_image_path(instance, filename):
-    directory = instance.name
-    return f'{directory}/{filename}'
 
 
 class Team(models.Model):
@@ -20,14 +17,13 @@ class Team(models.Model):
 
 
 class Player(models.Model):
-    name = models.CharField(verbose_name='선수 이름', max_length=100)
     team = models.ForeignKey(
         Team,
         on_delete=models.CASCADE,
     )
 
     # 선수 스탯
-    playin_time = models.PositiveSmallIntegerField(verbose_name='출장 시간', default=0)
+    playin_time = models.CharField(verbose_name='출장 시간', max_length=100)
     FGP = models.FloatField(verbose_name='야투 성공률', default=0)
     T3PP = models.FloatField(verbose_name='3점 야투 성공률', default=0)
     FTP = models.FloatField(verbose_name='자유투 성공률', default=0)
@@ -37,15 +33,15 @@ class Player(models.Model):
     BPG = models.FloatField(verbose_name='게임 평균 블락', default=0)
 
     # 선수 프로필
-    player_back_number = models.PositiveSmallIntegerField(verbose_name='선수 등 번호', default=0)
+    player_back_number = models.CharField(verbose_name='선수 등 번호', max_length=100)
     player_position = models.CharField(verbose_name='선수 포지션', max_length=5)
     player_first_name = models.CharField(verbose_name='선수 이름', max_length=50)
     player_last_name = models.CharField(verbose_name='선수 성씨', max_length=50)
-    player_height = models.PositiveSmallIntegerField(verbose_name='선수 키', default=0)
-    player_weight = models.PositiveSmallIntegerField(verbose_name='선수 무게', default=0)
+    player_height = models.CharField(verbose_name='선수 키', max_length=100)
+    player_weight = models.CharField(verbose_name='선수 무게', max_length=100)
     player_born = models.CharField(verbose_name='선수 생년월일', max_length=50)
     player_hometown = models.CharField(verbose_name='선수 고향', max_length=50)
-    player_nba_debut = models.PositiveSmallIntegerField(verbose_name='데뷔 년도', default=0)
+    player_nba_debut = models.CharField(verbose_name='데뷔 년도', max_length=50)
     player_image = models.ImageField(verbose_name='선수 사진', blank=True, null=True, default=True)
 
     def __str__(self):
--- crawler.py
+++ crawler.py
@@ -3,7 +3,7 @@ import time
 from bs4 import BeautifulSoup
 import urllib.request
 import os
-from members.models import *
+from members.models import Team , Player
 
 driver = webdriver.Chrome('/Users/mac/projects/ChromeWebDriver/chromedriver')
 
@@ -30,9 +30,88 @@ for url, name in zip (team_img_url, team_name):
     # Create target Directory
         os.mkdir(f'static/{dirName}')
         print("Directory ", dirName,  " Created ")
-
+        team = Team.objects.create(
+            name=name,
+            team_image=url,
+        )
 
     except FileExistsError:
         print("already exists ", dirName)
         pass
     urllib.request.urlretrieve(url, f'static/{name}/{name}.svg')
+
+for url, team_name in zip(detail_urls, team_name):
+    # 팀별 url 접근
+    driver.get(url)
+    time.sleep(2)
+
+    # 선수들 url 추출
+    all_players_urls = driver.find_elements_by_class_name("nba-player-index__trending-item")
+    player_url_a_tags = [url.find_elements_by_tag_name('a') for url in all_players_urls]
+    player_url = [url[0].get_attribute("href") for url in player_url_a_tags]
+
+    # 선수별 url 접근
+    for url in player_url:
+        driver.get(url)
+        time.sleep(2)
+
+        player_img_url_section = driver.find_elements_by_xpath \
+            ('//*[@id="block-league-content"]/player-detail/section[1]/header/section[1]/img')
+        player_img_src = player_img_url_section[0].get_attribute("src")
+        season_stat_tbody= driver.find_elements_by_xpath \
+            ('//*[@id="block-league-content"]/player-detail/player-snapshot-career/section/table/tbody')
+        tr_data = season_stat_tbody[0].find_elements_by_tag_name('tr')
+        tr_data[0].find_elements_by_tag_name('td')
+
+
+        player_stats = [ url.get_attribute("innerText") for url in tr_data[0].find_elements_by_tag_name('td')]
+        MPG = player_stats[0]
+        FGP = player_stats[1]
+        T3PP = player_stats[2]
+        FTP = player_stats[3]
+        PPG = player_stats[4]
+        RPG = player_stats[5]
+        APG = player_stats[6]
+        BPG = player_stats[7]
+
+        player_back_number = driver.find_element_by_xpath('//*[@id="block-league-content"]/player-detail/section[1]/header/section[2]/p/span[1]').get_attribute("innerText")[1:]
+        player_position = driver.find_element_by_xpath('//*[@id="block-league-content"]/player-detail/section[1]/header/section[2]/p/span[3]').get_attribute('innerText')
+        player_first_name = driver.find_element_by_xpath('//*[@id="block-league-content"]/player-detail/section[1]/header/section[2]/section/p[1]').get_attribute('innerText')
+        player_last_name = driver.find_element_by_xpath('//*[@id="block-league-content"]/player-detail/section[1]/header/section[2]/section/p[2]').get_attribute('innerText')
+        player_height = driver.find_element_by_xpath('//*[@id="player-tabs-Info"]/section/section[1]/section[1]/section[1]/p[3]').get_attribute('innerText')[2:]
+        player_weight = driver.find_element_by_xpath('//*[@id="player-tabs-Info"]/section/section[1]/section[1]/section[2]/p[3]').get_attribute('innerText')[2:]
+        player_born = driver.find_element_by_xpath('//*[@id="player-tabs-Info"]/section/section[1]/section[2]/ul/li[1]/span[2]').get_attribute('innerText')
+        player_hometown = driver.find_element_by_xpath('//*[@id="player-tabs-Info"]/section/section[1]/section[2]/ul/li[3]/span[2]').get_attribute('innerText')
+        player_nba_debut = driver.find_element_by_xpath('//*[@id="player-tabs-Info"]/section/section[1]/section[2]/ul/li[4]/span[2]').get_attribute('innerText')
+
+        try:
+            player_path = player_back_number + " " + player_first_name + " " + player_last_name
+            urllib.request.urlretrieve(player_img_src, f'static/{team_name}/{player_path}.png')
+
+            player_first_name = Player.objects.create(
+                # MPG 를 pycharm 에서 playin_time으로 작성함
+
+                playin_time=MPG,
+                FGP=FGP,
+                T3PP=T3PP,
+                FTP=FTP,
+                PPG=PPG,
+                RPG=RPG,
+                APG=APG,
+                BPG=BPG,
+
+                player_back_number=player_back_number,
+                player_position=player_position,
+                player_first_name=player_first_name,
+                player_last_name=player_last_name,
+                player_height=player_height,
+                player_weight=player_weight,
+                player_born=player_born,
+                player_hometown=player_hometown,
+                player_nba_debut=player_nba_debut,
+            )
+            print(player_path, " Created ===========")
+
+        except FileExistsError:
+            print("already exists ", player_path)
+            pass
