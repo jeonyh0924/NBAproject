7da79400be0fffe38531a7ecbdac5c7990f43fbc
Jeon Young Hoon <hungyb0924@gmail.com>
2019-02-20 02:05:08 +0900

    190220 크롤링 클래스 분리 / 다음 목표  클래스 매니저로 클래스 관리하기

--- app/members/models.py
+++ app/members/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.core.files import File
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+
 
 
 class User(AbstractUser):
@@ -24,6 +26,51 @@ class Team(models.Model):
 
     def __str__(self):
         return self.name
+
+    @staticmethod
+    def crawler():
+
+        from selenium import webdriver
+        import time
+        import urllib.request
+        import os
+        from members.models import Team
+        from django.core.exceptions import ObjectDoesNotExist
+
+        driver = webdriver.Chrome('/Users/mac/projects/ChromeWebDriver/chromedriver')
+
+        driver.implicitly_wait(3)
+
+        driver.get('http://www.nba.com/teams/')
+        time.sleep(3)
+
+        team_url_list = driver.find_elements_by_xpath("//div/div/div[@class='team__list']/a")
+        team_name = [url.get_attribute("innerText") for url in team_url_list]
+        team_img_urls = driver.find_elements_by_xpath("//div/div/div[@class='team__list']/img")
+        team_img_url = [url.get_attribute("src") for url in team_img_urls]
+
+        for url, name in zip(team_img_url, team_name):
+            dirName = name
+            try:
+                # Create target Directory
+                os.mkdir(f'static/{dirName}')
+
+                urllib.request.urlretrieve(url, f'static/{name}/{name}.svg')
+                f = open(os.path.join(settings.BASE_DIR, f'static/{name}/{name}.svg'), 'rb')
+
+                team = Team.objects.create(
+                    name=name,
+                    team_image=File(f),
+                )
+                print("Directory ", dirName, " Created ")
+                f.close()
+
+            except FileExistsError:
+                print("already exists ", dirName)
+
+        driver.close()
+
+
 
 
 class Player(models.Model):
@@ -62,10 +109,10 @@ class Player(models.Model):
     def crawler():
         from selenium import webdriver
         import time
-        from bs4 import BeautifulSoup
         import urllib.request
         import os
         from members.models import Team, Player
+        from django.core.exceptions import ObjectDoesNotExist
 
         driver = webdriver.Chrome('/Users/mac/projects/ChromeWebDriver/chromedriver')
 
@@ -78,34 +125,13 @@ class Player(models.Model):
         team_url_list = driver.find_elements_by_xpath("//div/div/div[@class='team__list']/a")
         detail_urls = [url.get_attribute("href") for url in team_url_list]
 
-        ### 각 팀별 이름 출력
-        team_name = [url.get_attribute("innerText") for url in team_url_list]
+        try:
+            Teams = Team.objects.get(pk=1)
+            Teams = Team.objects.all()
+        except ObjectDoesNotExist:
+            Teams.crawler()
+            Teams= Team.objects.all()
 
-        ### 각 팀별 이미지 출력
-        team_img_urls = driver.find_elements_by_xpath("//div/div/div[@class='team__list']/img")
-        team_img_url = [url.get_attribute("src") for url in team_img_urls]
-
-        for url, name in zip(team_img_url, team_name):
-            dirName = name
-            try:
-                # Create target Directory
-                os.mkdir(f'static/{dirName}')
-
-                urllib.request.urlretrieve(url, f'static/{name}/{name}.svg')
-                f = open(os.path.join(settings.BASE_DIR, f'static/{name}/{name}.svg'), 'rb')
-
-                team = Team.objects.create(
-                    name=name,
-                    team_image=File(f),
-                )
-                print("Directory ", dirName, " Created ")
-                f.close()
-
-            except FileExistsError:
-                print("already exists ", dirName)
-                pass
-
-        Teams = Team.objects.all()
 
         for index, url in enumerate(detail_urls):
             # 팀별 url 접근
@@ -202,7 +228,6 @@ class Player(models.Model):
                     )
                     f.close()
                     print(player_path, " Created ===========")
-
                 except FileExistsError:
                     print("already exists ", player_path)
                     pass
