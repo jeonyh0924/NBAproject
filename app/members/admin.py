b21f32a337774c76e1f0f2895ab8d895561473d6
Jeon Young Hoon <hungyb0924@gmail.com>
2019-02-19 16:58:01 +0900

    190219 admin register 수정

--- app/members/admin.py
+++ app/members/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from .models import *
 
+
 # Register your models here.
-admin.site.register(Team)
-admin.site.register(Player)
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+    ordering = ['pk']
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'back_number', 'position', 'team']
+    list_per_page = 40
+    list_filter = ['team', 'id']
+    ordering = ['pk']
