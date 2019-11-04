3503459a662d66d284c9369546865f87b2066cd3
Jeon Young Hoon <hungyb0924@gmail.com>
2019-11-05 01:39:56 +0900

    challenge post 완료 및 index.html 가장 인기 있는 선수 표시 완료 15dollars challenge 인기글 완성

--- app/challengepost/urls.py
+++ app/challengepost/urls.py
@@ -5,7 +5,7 @@ app_name = 'challengepost'
 
 urlpatterns = [
     path('', views.post_list, name='list'),
-    path('<int:post_pk>/', views.post_detail, name='detail'),
+    path('detail/(?P<post_pk>[0-9]+)/', views.post_detail, name='detail'),
     path('<int:post_pk>/comments/create/', views.comment_create, name='comment-create'),
     path('create/', views.post_create, name='create'),
     path('<int:post_pk>/like-toggle/', views.post_like_toggle, name='post-like-toggle'),
--- app/config/views.py
+++ app/config/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 
+from challengepost.models import challengePost
 from members.models import Player
 from posts.models import Post
 
@@ -18,6 +19,21 @@ def index(request):
 
     best_posts = post_list[0:3]
 
+    # 15 dollars challenge best posts
+    challenge_post_list_query = challengePost.objects.all()
+    challenge_posts_list = []
+
+    for post in challenge_post_list_query:
+        challenge_posts_list.append(post)
+
+    for i in range(1, len(challenge_posts_list)):
+        for j in range(0, len(challenge_posts_list) - 1):
+            if challenge_posts_list[j].like_users.count() < challenge_posts_list[j + 1].like_users.count():
+                challenge_posts_list[j + 1], challenge_posts_list[j] = challenge_posts_list[j], challenge_posts_list[
+                    j + 1]
+
+    challenge_posts_list = challenge_posts_list[0:2]
+
     # best_player
     best_player = Player.objects.first()
     player_query = Player.objects.all()
@@ -33,5 +49,6 @@ def index(request):
     context = {
         'best_posts': best_posts,
         'best_player': best_player,
+        'challenge_posts_list': challenge_posts_list,
     }
     return render(request, 'base.html', context)
